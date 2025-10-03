7bbf7fca4e343f0eaec077a2c11a0a4db6258beb
Azhar Firdaus <azhar.firdaus9@gmail.com>
2025-10-03 13:49:30 +0700

    added comment

--- undirected_v2/graph.go
+++ undirected_v2/graph.go
@@ -8,6 +8,14 @@ type Graph[T any] struct {
 	degreeSize int
 }
 
+// build graph by given size and array of vertices
+// time-complexity O(V)
+//
+//	where V is the number of vertices
+//
+// space-complexity O(V)
+//
+//	where V is the number of vertices
 func Build[T any](size int, vertices []*Vertex[T]) (*Graph[T], error) {
 	if size != len(vertices) {
 		return nil, errors.New("defined size is not equal to vertices size")
@@ -32,10 +40,16 @@ func (g *Graph[T]) Size() int {
 	return g.size
 }
 
+// get number of degree
+// time-complexity O(1)
+// space-complexity O(1)
 func (g *Graph[T]) GetDegreeSize() int {
 	return g.degreeSize
 }
 
+// get number of edge
+// time-complexity O(1)
+// space-complexity O(1)
 func (g *Graph[T]) GetEdgeSize() int {
 	return g.degreeSize / 2
 }
